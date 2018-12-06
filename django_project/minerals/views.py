931342a83057856f5a5324c3c37fbdf2aa48fadb
Hoessein Abdullahi <habdullahi@saa.nl>
2018-12-06 23:05:44 +0100

    Messing around with loops in detail page

--- django_project/minerals/views.py
+++ django_project/minerals/views.py
@@ -9,7 +9,17 @@ def home(request):
 
 def mineral_detail(request, pk):
     mineral = Mineral.objects.filter(pk=pk)
-    return render(request, 'minerals/detail.html', {'mineral': mineral})
+
+    d = {}
+    for x in Mineral._meta.get_fields():
+        # if x.name != 'id' or 'name' or 'image_filename':
+        d[x.name] = getattr(Mineral, x.name)
+        # else:
+        #     pass
+
+
+    return render(request, 'minerals/detail.html', {'mineral': mineral,
+                                                    'd': d})
 
 
 if __name__ == '__main__':
