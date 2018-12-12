38ff0dd7ba2ed859280ee657f2bfb5f5c7a1b5c5
Hoessein Abdullahi <habdullahi@saa.nl>
2018-12-12 20:04:11 +0100

    added missing minerals to model

--- django_project/minerals/models.py
+++ django_project/minerals/models.py
@@ -1,27 +1,26 @@
 from django.db import models
 import json
 
-
 class Mineral(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    image_filename = models.CharField(max_length=255)
-    image_caption = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
-    formula = models.CharField(max_length=255)
-    strunz_classification = models.CharField(max_length=255)
-    color = models.CharField(max_length=255)
-    crystal_system = models.CharField(max_length=255)
-    unit_cell = models.CharField(max_length=255)
-    crystal_symmetry = models.CharField(max_length=255)
-    cleavage = models.CharField(max_length=255)
-    mohs_scale_hardness = models.CharField(max_length=255)
-    luster = models.CharField(max_length=255)
-    streak = models.CharField(max_length=255)
+    name = models.CharField(max_length=500)
+    image_filename = models.CharField(max_length=500)
+    image_caption = models.CharField(max_length=500)
+    category = models.CharField(max_length=500)
+    formula = models.CharField(max_length=500)
+    strunz_classification = models.CharField(max_length=500)
+    color = models.CharField(max_length=500)
+    crystal_system = models.CharField(max_length=500)
+    unit_cell = models.CharField(max_length=500)
+    crystal_symmetry = models.CharField(max_length=500)
+    cleavage = models.CharField(max_length=500)
+    mohs_scale_hardness = models.CharField(max_length=500)
+    luster = models.CharField(max_length=500)
+    streak = models.CharField(max_length=500)
     diaphaneity = models.CharField(max_length=255)
-    optical_properties = models.CharField(max_length=255)
-    refractive_index = models.CharField(max_length=255)
-    crystal_habit = models.CharField(max_length=255)
-    specific_gravity = models.CharField(max_length=255)
+    optical_properties = models.CharField(max_length=500)
+    refractive_index = models.CharField(max_length=500)
+    crystal_habit = models.CharField(max_length=500)
+    specific_gravity = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
@@ -34,25 +33,24 @@ class Mineral(models.Model):
                 try:
                     Mineral(
                         name=mineral['name'],
-                        image_filename=mineral['image filename'],
-                        image_caption=mineral['image caption'],
-                        category=mineral['category'],
-                        formula=mineral['formula'],
-                        strunz_classification=mineral['strunz classification'],
-                        crystal_system=mineral['crystal system'],
-                        unit_cell=mineral['unit cell'],
-                        color=mineral['color'],
-                        crystal_symmetry=mineral['crystal symmetry'],
-                        cleavage=mineral['cleavage'],
-                        mohs_scale_hardness=mineral['mohs scale hardness'],
-                        luster=mineral['luster'],
-                        streak=mineral['streak'],
-                        diaphaneity=mineral['diaphaneity'],
-                        optical_properties=mineral['optical properties'],
-                        refractive_index=mineral['refractive index'],
-                        crystal_habit=mineral['crystal habit'],
-                        specific_gravity=mineral['specific gravity']
+                        image_filename=mineral.get('image filename', ""),
+                        image_caption=mineral.get('image caption', ""),
+                        category=mineral.get('category',""),
+                        formula=mineral.get('formula', ""),
+                        strunz_classification=mineral.get('strunz classification', ""),
+                        crystal_system=mineral.get('crystal system', ""),
+                        unit_cell=mineral.get('unit cell', ""),
+                        color=mineral.get('color', ""),
+                        crystal_symmetry=mineral.get('crystal symmetry', ""),
+                        cleavage=mineral.get('cleavage', ""),
+                        mohs_scale_hardness=mineral.get('mohs scale hardness', ""),
+                        luster=mineral.get('luster', ""),
+                        streak=mineral.get('streak', ""),
+                        diaphaneity=mineral.get('diaphaneity', ""),
+                        optical_properties=mineral.get('optical properties', ""),
+                        refractive_index=mineral.get('refractive index', ""),
+                        crystal_habit=mineral.get('crystal habit', ""),
+                        specific_gravity=mineral.get('specific gravity', "")
                     ).save()
                 except KeyError:
-                    pass
-
+                    continue
