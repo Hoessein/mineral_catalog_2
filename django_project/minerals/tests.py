4d7287ae91a6cec5d30c171e08479db699d3a65d
Hoessein Abdullahi <habdullahi@saa.nl>
2018-12-13 23:28:30 +0100

    added mineral model test

--- django_project/minerals/tests.py
+++ django_project/minerals/tests.py
@@ -1,3 +1,32 @@
+from django.urls import reverse
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Mineral
+
+
+class CourseModelTests(TestCase):
+    def test_course_creation(self):
+        mineral=Mineral.objects.create(
+            name='name',
+            image_filename='image filename',
+            image_caption='image caption',
+            category='category',
+            formula='formula',
+            strunz_classification='strunz classification',
+            crystal_system='crystal system',
+            unit_cell='unit cell',
+            color='color',
+            crystal_symmetry='crystal symmetry',
+            cleavage='cleavage',
+            mohs_scale_hardness='mohs scale hardness',
+            luster='luster',
+            streak='streak',
+            diaphaneity='diaphaneity',
+            optical_properties='optical properties',
+            refractive_index='refractive index',
+            crystal_habit='crystal habit',
+            specific_gravity='specific gravity',
+            group='group'
+    )
+        self.assertEqual(mineral.name, 'namadfadfe')
