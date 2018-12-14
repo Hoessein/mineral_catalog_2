2346a90b33d4bf80c8c2b8cd5d1c15e52055a25d
Hoessein Abdullahi <habdullahi@saa.nl>
2018-12-14 16:27:14 +0100

    data migration added

--- django_project/minerals/models.py
+++ django_project/minerals/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import json
+
 
 class Mineral(models.Model):
     name = models.CharField(max_length=500)
@@ -25,34 +25,3 @@ class Mineral(models.Model):
 
     def __str__(self):
         return self.name
-
-    @classmethod
-    def json_to_db(cls):
-        with open('minerals.json', encoding='utf8') as file:
-            data = json.load(file)
-            for mineral in data:
-                try:
-                    Mineral(
-                        name=mineral['name'],
-                        image_filename=mineral.get('image filename', ""),
-                        image_caption=mineral.get('image caption', ""),
-                        category=mineral.get('category',""),
-                        formula=mineral.get('formula', ""),
-                        strunz_classification=mineral.get('strunz classification', ""),
-                        crystal_system=mineral.get('crystal system', ""),
-                        unit_cell=mineral.get('unit cell', ""),
-                        color=mineral.get('color', ""),
-                        crystal_symmetry=mineral.get('crystal symmetry', ""),
-                        cleavage=mineral.get('cleavage', ""),
-                        mohs_scale_hardness=mineral.get('mohs scale hardness', ""),
-                        luster=mineral.get('luster', ""),
-                        streak=mineral.get('streak', ""),
-                        diaphaneity=mineral.get('diaphaneity', ""),
-                        optical_properties=mineral.get('optical properties', ""),
-                        refractive_index=mineral.get('refractive index', ""),
-                        crystal_habit=mineral.get('crystal habit', ""),
-                        specific_gravity=mineral.get('specific gravity', ""),
-                        group=mineral.get('group')
-                    ).save()
-                except KeyError:
-                    continue
